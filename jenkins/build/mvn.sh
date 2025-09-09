bf48f78aca92a67ccc4afde4ac004fa9e5855fff
DangKhoa <khoanguyendang@zigexn.vn>
2025-09-09 21:59:55 +0700

    fix: Fix 4

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -5,6 +5,6 @@ echo "** Building jar ***********************************"
 echo "***************************************************"
 
 WORKSPACE=/home/khoand/jenkins-data/jenkins_home/workspace/pipeline-docker-mavenpi
-docker run --rm   -v "$WORKSPACE /java-app":/app   -v "$HOME/.m2":/root/.m2   -w /app   maven:latest "$@"
+docker run --rm   -v "$WORKSPACE/java-app":/app   -v "$HOME/.m2":/root/.m2   -w /app   maven:latest "$@"
 
 
