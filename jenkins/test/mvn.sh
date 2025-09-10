12097cb342e107d4f14205e02caf15d3443216ea
DangKhoa <khoanguyendang@zigexn.vn>
2025-09-10 08:41:14 +0700

    fix 10

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -4,7 +4,7 @@ echo "***************************************************"
 echo "** Testing the code ***********************************"
 echo "***************************************************"
 
-WORKSPACE=/home/khoand/jenkins-data/jenkins_home/workspace/pipeline-docker-mavenpi
+WORKSPACE=/home/khoand/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
 docker run --rm   -v "$WORKSPACE/java-app":/app   -v /home/khoand/jenkins-data/jenkins_home/.m2:/root/.m2   -w /app   maven:latest "$@"
 
 
